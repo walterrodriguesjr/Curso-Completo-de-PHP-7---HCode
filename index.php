43e318b220373a9d66f08319a791131314cba57f
walter <walterrjr.86@gmail.com>
2022-02-09 16:43:32 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 
-<<<<<<< HEAD
+
 echo "teste novo";
 echo "teste 1";
 echo "teste 2";
-=======
+
 echo "Testando hcode";
 
 
 echo "novo";
->>>>>>> 0066fc54d7399eb83ebec716097a34fb543f7ecc
+
 
 ?>
