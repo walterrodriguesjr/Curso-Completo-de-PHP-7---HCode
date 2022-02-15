932bd0c2543a490a7a402201a4219395dcad9d41
walter <walterrjr.86@gmail.com>
2022-02-15 18:24:33 -0300

    'atualizando'

--- secao 12-banco de dados-PDO/61/61.php
+++ secao 12-banco de dados-PDO/61/61.php
@@ -1,5 +1,19 @@
-<?php 
+<?php
 
-    
+$conn = new PDO("mysql:host=localhost; dbname=dbphp7", "root", "");
 
-?>
+
+$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
+
+$login = "joão";
+$password = "66666666";
+$id = 2;
+
+$stmt->bindParam(":LOGIN", $login);
+$stmt->bindParam(":PASSWORD", $password);
+$stmt->bindParam(":ID", $id);
+
+$stmt->execute();
+
+echo "alterando OK";
+
