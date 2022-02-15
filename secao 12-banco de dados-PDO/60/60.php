c2ec719b6940581a8ddcd81cd7efbffd5cb6bf53
walter <walterrjr.86@gmail.com>
2022-02-15 18:00:21 -0300

    'atualizando'

--- secao 12-banco de dados-PDO/60/60.php
+++ secao 12-banco de dados-PDO/60/60.php
@@ -1,5 +1,17 @@
-<?php 
+<?php
+
+$conn = new PDO("mysql:host=localhost; dbname=dbphp7", "root", "");
 
 
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
 
-?>
+$login = "jose";
+$password = "1234567890";
+
+$stmt->bindParam(":LOGIN", $login);
+$stmt->bindParam(":PASSWORD", $password);
+
+$stmt->execute();
+
+echo "Inserido OK";
+
